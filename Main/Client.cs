ad2576f9c1f2ee3a85bf0d4d3414e008ae689cdd
ak47007tiger <ak47007tiger@163.com>
2018-09-12 09:22:18 +0800

    normal commit

--- Main/Client.cs
+++ Main/Client.cs
@@ -5,6 +5,13 @@ using LeetCode.Main.Toolkit;
 namespace LeetCode.Main {
   public class Client : LeetCodeSolution{
     public static void Work(string[] args) {
+      ClimbStairs climbStairs = new ClimbStairs();
+      print(climbStairs.Solution1(5));
+      print(climbStairs.Solution2(5));
+      print(climbStairs.Solution2(4));
+      print(climbStairs.Solution3(3));
+      print(climbStairs.Solution3(4));
+      print(climbStairs.Solution3(5));
       Console.ReadKey();
     }
   }
