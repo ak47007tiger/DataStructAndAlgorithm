e07e3c3049250606fade7b11875f82d7d1cef895
ak47007tiger <ak47007tiger@163.com>
2019-05-07 07:14:28 +0800

    new files

---
+++ DataStructureAndAlgorithm/Editor/FileTemplateEditor.cs
@@ -0,0 +1,11 @@
+namespace DataStructure.Editor
+{
+  using System.Collections;
+  using System.Collections.Generic;
+    public class FileTemplateEditor
+    {
+      public void CreateDirs(string parent, List<string> paths){
+        
+      }
+    }
+}
