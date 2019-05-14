71d1a64814f3a3b71ffdb8bc007affda858b679e
ak47007tiger <ak47007tiger@163.com>
2019-05-14 11:57:36 +0800

    auto

--- DataStructureAndAlgorithm/Client.cs
+++ DataStructureAndAlgorithm/Client.cs
@@ -7,23 +7,33 @@ using LeetCode.Hard;
 using LeetCode.Medium;
 using NiuKeWang;
 
-namespace DataStructure {
-  public class Client : BaseSolution {
-    public static void Work(string[] args) {
-      var array = new int[]{2,4,6,7,8,3,2,5};
-      var solution = new ArrayMaxHeap();
-      solution.Build(array.Length);
-      foreach(var num in array){
-        solution.Insert(num);
-        print(solution.GetTop());
-        print(',');
-      }
-      println();
-      while(solution.GetSize() > 0){
-        print(solution.RemoveTop());
-        print(',');
-      }
-      Console.ReadKey();
+namespace DataStructure
+{
+    public class Client : BaseSolution
+    {
+        public static void TestArrayMaxHeap()
+        {
+            var array = new int[] { 2, 4, 6, 7, 8, 3, 2, 5 };
+            var solution = new ArrayMaxHeap();
+            solution.Build(array.Length);
+            foreach (var num in array)
+            {
+                solution.Insert(num);
+                print(solution.GetTop());
+                print(',');
+            }
+            println();
+            while (solution.GetSize() > 0)
+            {
+                print(solution.RemoveTop());
+                print(',');
+            }
+        }
+
+        public static void Work(string[] args)
+        {
+            printArray(new ShellSort().Sort(new int[]{3,1,66,23,9,15,8,6,70}));
+            Console.ReadKey();
+        }
     }
-  }
 }
---
+++ DataStructureAndAlgorithm/DataStructure/Sort/InsertionSort.cs
@@ -0,0 +1,29 @@
+namespace DataStructure
+{
+    public class InsertionSort
+    {
+        /*
+        从后往前，像是拿一张扑克牌放到手里排序手里的牌
+        认为第一张是排好序的，从后面那数值插入到排好序的序列里
+         */
+        public int[] Sort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp = array[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (array[j] > temp)
+                    {
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                    else
+                        break;
+                }
+            }
+            return array;
+        }
+    }
+
+}
