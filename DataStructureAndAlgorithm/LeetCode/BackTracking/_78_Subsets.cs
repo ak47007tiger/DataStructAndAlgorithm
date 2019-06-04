87896457c66a9c9d53cc0a0cdbb86d327638f939
ak47007tiger <ak47007tiger@163.com>
2019-06-04 21:18:18 +0800

    auto

---
+++ DataStructureAndAlgorithm/LeetCode/BackTracking/_78_Subsets.cs
@@ -0,0 +1,44 @@
+namespace LeetCode
+{
+  using System;
+  using System.Collections;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using DataStructure;
+  using DataStructure.Toolkit;
+
+  /*
+  Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+[3],
+[1],
+[2],
+[1,2,3],
+[1,3],
+[2,3],
+[1,2],
+[]
+]
+   */
+
+/*
+用0-n个元素可以组成的不重复的集合数量
+ */
+  public class SubsetsCls : BaseSolution
+  {
+
+    public IList<IList<int>> Subsets(int[] nums)
+    {
+      
+      return null;
+    }
+  }
+}
