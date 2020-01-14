50c96ce94b09f3febffd9b598b0ed26b454f9154
ak47007tiger <ak47007tiger@163.com>
2020-01-14 18:02:18 +0800

    auto

--- DataStructureAndAlgorithm/Test/Language/TestAsyncAwaitWithComplete.cs
+++ DataStructureAndAlgorithm/Test/Language/TestAsyncAwaitWithComplete.cs
@@ -1,22 +1,48 @@
+using System.Threading;
 using System.Threading.Tasks;
 namespace Test
 {
+    /*
+    线程a：
+        异步调用1：调用方法1
+        异步调用2：调用方法1
+        方法1：根据变量走2个分支
+
+    */
     public class TestAsyncAwaitWithComplete
     {
         public int counter;
 
-        public async void Add1()
-        {
+        public async void WorkFunc1(){
+            await Task.Factory.StartNew(()=>{
+                Thread.Sleep(100);
+            });
+        }
+
+        public async void WorkFunc2(){
+            await Task.Factory.StartNew(()=>{
+                Thread.Sleep(100);
+            });
+        }
+
+        public async void Call(){
             await Task.Factory.StartNew(()=>{
                 
             });
         }
 
-        public async void Add2()
+        public async void Add1()
+        {
+            await Task.Factory.StartNew(()=>{
+                counter += 1;
+            });
+        }
+
+        public async void Minus1()
         {
             await Task.Run(() =>
             {
-
+                counter -= 1;
             });
         }
     }
